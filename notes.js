f91c0983871da71498c495ad55c1adeb1435acb3
Mohamed Salah <lafineequipedu2@gmail.com>
2023-01-24 11:11:20 +0100

    first comit

---
+++ notes.js
@@ -0,0 +1,4 @@
+// npm init -y
+// npm i express mongoose 
+// npm i --save-dev dotenv nodemon
+//  in package.json -> scripts -> devStart : nodemon server.js
---
+++ script.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+
+app.listen(3000, ()=>{console.log("server running on port 3000")})
